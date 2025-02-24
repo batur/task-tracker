72f2bb33016ecce29ce852824d79c762b1119d2a
Batur <halidbaturakkurt@gmail.com>
2025-02-24 22:17:10 +0300

    feat: enhance Swagger documentation with versioning and operation IDs for tasks

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Task Tracker API')
     .setDescription('The Task Tracker API description')
+    .setVersion('1.0')
+    .addTag('tasks')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: '/api-json',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,12 @@ import {
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { TaskEntity } from './entities/task.entity';
 
 @Controller('tasks')
@@ -19,30 +24,35 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
+  @ApiOperation({ operationId: 'createTask' })
   @ApiCreatedResponse({ type: TaskEntity })
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
   }
 
   @Get()
+  @ApiOperation({ operationId: 'findAllTasks' })
   @ApiOkResponse({ type: TaskEntity, isArray: true })
   findAll() {
     return this.tasksService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ operationId: 'findTaskById' })
   @ApiOkResponse({ type: TaskEntity })
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ operationId: 'updateTask' })
   @ApiOkResponse({ type: TaskEntity })
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(+id, updateTaskDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ operationId: 'removeTask' })
   @ApiOkResponse({ type: TaskEntity })
   remove(@Param('id') id: string) {
     return this.tasksService.remove(+id);
